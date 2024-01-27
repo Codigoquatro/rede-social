50f2e9a8ea02480152fc353145e7d1d4ea548452
CodigoQuatro <codigoquatro2022@gmail.com>
2024-01-27 12:15:42 -0300

    criado modelo de usuario

---
+++ src/models/Usuario.go
@@ -0,0 +1,10 @@
+package models
+
+type Usuario struct {
+	ID       uint64 `json:"id,omitempty"`
+	Nome     uint64 `json:"nome,omitempty"`
+	Nick     uint64 `json:"nick,omitempty"`
+	Email    uint64 `json:"email,omitempty"`
+	Senha    uint64 `json:"senha,omitempty"`
+	CriadoEm uint64 `json:"CriadoEm,omitempty"`
+}
