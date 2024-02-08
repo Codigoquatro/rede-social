e1f8fc9f2b7757669eeb66c8d4394bd6a5ae9320
CodigoQuatro <codigoquatro2022@gmail.com>
2024-02-08 10:00:38 -0300

    criado permissoes usuarios

---
+++ sql/dados.sql
@@ -0,0 +1,5 @@
+insert into seguidores(usuario_id,seguidor_id)
+values
+(1.2)
+(3,1)
+(1,3)
--- sql/sql.sql
+++ sql/sql.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS rede;
 USE rede;
 
 DROP TABLE IF EXISTS usuarios;
-
+DROP TABLE IF EXISTS seguidores;
 
 CREATE TABLE usuarios(
     id int auto_increment primary key,
@@ -11,4 +11,18 @@ CREATE TABLE usuarios(
     email varchar(50) not null unique,
     senha varchar(50) not null unique,
     criadoEm timestamp default current_
+)ENGINE=INNODB;
+
+CREATE TABLE seguidores(
+    usario_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    
+    PRIMARY KEY (usuario_id,seguidor_id)
 )ENGINE=INNODB;
